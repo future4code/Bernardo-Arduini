a196e50111b09f044b3ccd6cf45050a74bea8195
Bernardo Arduini <berzin-@hotmail.com>
2020-04-01 16:47:41 -0300

    subindo aula semana3-aula11

---
+++ semana3/loops/index.js
@@ -0,0 +1,160 @@
+// let array = [1, 3, 5, 7];
+// array.push(9);
+// console.log(array)
+
+
+// let i = 0
+
+// while(i < 10){
+//     console.log(i);
+
+//     i++;
+// }
+
+
+// let = estouComFome = true
+// let = estomago = 0
+
+// while(estouComFome){
+//     console.log("Quero comer mais cochinhas", estomago);
+//     estomago = estomago + 10;
+
+//     if(estomago === 100){
+//         estouComFome = false
+//     }
+// }
+
+
+
+// let estomago = 0
+
+// while(estomago < 100){
+//     console.log("Quero comer mais cochinhas", estomago);
+//     estomago = estomago + 10
+// }
+
+
+
+// let numeroDoUsuario = Number(prompt("Digite um numero"));
+// let soma = 0
+// while(numeroDoUsuario !== 0){
+
+//     soma = soma + numeroDoUsuario
+//     numeroDoUsuario = Number(prompt("Digite um numero"))
+
+// }
+
+// console.log("A soma e ", soma)
+
+
+
+// \/ numeros pares e impares
+
+// for(let i = 0; i <= 10; i++){
+// // console.log(i)
+// if(i % 2 === 0){
+//     console.log(i, " e par");
+// }else{
+//     console.log(i+" e impar");
+// }
+// }
+
+
+// const numeros = [14, 67, 89, 15, 23]
+// for(let i = 0; i<5; i++){
+//     const elemento = numeros[i];
+//     console.log(elemento)
+// }
+
+
+
+// 
+
+// const numeros = [11, 15, 18, 14, 12, 13, 22]
+// let maior = numeros[0];
+
+// for(let i = 0; i < 7; i++){
+//     const elemento = numeros[i];
+//     if(elemento > maior){
+//         maior = elemento;
+//         }
+// }
+// console.log("O mairo numero e "+maior)
+
+
+// const arrayPalavra = ["oi", "sumida", "tudo", "bem?", "sdds"]
+// let mensagem = "";
+
+// for(let palavra of arrayPalavra){
+//     mensagem = mensagem + palavra + " ";
+// }
+// ----------------------------------------------------------
+// // console.log(mensagem)
+
+//  const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+//  const novaLista = []
+//  const numero = 3
+//  for(const item of lista){
+//    if(item%numero === 0) {
+//      novaLista.push(item)
+//    }                                            //[12, 15, 18, 21, 27, 30]
+//  }
+//  console.log(novaLista)
+
+// // console.log(mensagem)
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// const novaLista = []
+// const numero = 4
+// for(const item of lista){
+//   if(item%numero === 0) {
+//     novaLista.push(item)
+//   }                                           //[12]
+// }
+// console.log(novaLista)
+
+
+//(3 - B)
+
+//  const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//  let maior = numeros[0];
+
+//  for(let i = 0; i < 12; i++){
+//      const elemento = numeros[i];
+//      if(elemento > maior){
+//          maior = elemento;
+//      }
+//  }
+//  console.log("O maior numero e "+maior)
+
+
+// const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// let menor = numeros[0];
+
+// for(let i = 0; i < 12; i++){
+//     const elemento = numeros[i];
+//     if(elemento > menor){
+//         menor = elemento;
+//     }
+// }
+// console.log("O menor numero e "+menor)
+
+
+// //(3 - B)
+
+// i dont know
+
+//(3 - C)
+
+//  const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+//   for(const i = 0; i < 12; i++){
+//   if(i % 2 === 0){
+//       console.log(i, " e par");
+//   }
+//   }
+
+
+//(3 - D)
+
+
