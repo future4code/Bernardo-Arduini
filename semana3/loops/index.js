1d98e2378d3ef438a8a13c101f322b0afc3e303d
Bernardo Arduini <berzin-@hotmail.com>
2020-04-01 16:50:31 -0300

    subindo

---
+++ semana3/loops/index.js
@@ -0,0 +1,71 @@
+
+// ----------------------------------------------------------
+// // console.log(mensagem)
+
+//  const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+//  const novaLista = []
+//  const numero = 3
+//  for(const item of lista){
+//    if(item%numero === 0) {
+//      novaLista.push(item)
+//    }                                            //[12, 15, 18, 21, 27, 30]
+//  }
+//  console.log(novaLista)
+
+// // console.log(mensagem)
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// const novaLista = []
+// const numero = 4
+// for(const item of lista){
+//   if(item%numero === 0) {
+//     novaLista.push(item)
+//   }                                           //[12]
+// }
+// console.log(novaLista)
+
+
+//(3 - B)
+
+//  const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+//  let maior = numeros[0];
+
+//  for(let i = 0; i < 12; i++){
+//      const elemento = numeros[i];
+//      if(elemento > maior){
+//          maior = elemento;
+//      }
+//  }
+//  console.log("O maior numero e "+maior)
+
+
+// const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// let menor = numeros[0];
+
+// for(let i = 0; i < 12; i++){
+//     const elemento = numeros[i];
+//     if(elemento > menor){
+//         menor = elemento;
+//     }
+// }
+// console.log("O menor numero e "+menor)
+
+
+// //(3 - B)
+
+// i dont know
+
+//(3 - C)
+
+//  const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+//   for(const i = 0; i < 12; i++){
+//   if(i % 2 === 0){
+//       console.log(i, " e par");
+//   }
+//   }
+
+
+//(3 - D)
+
+
